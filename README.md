6d513b9506795077b966ae34eb750f3ada5d59d5
huynguyen-py <iamhuynguyen1002@gmail.com>
2021-08-05 10:06:38 +0700

    Confirm task

---
+++ README.md
@@ -0,0 +1,31 @@
+﻿## FERDON_Interview_RL
+#### Reinforcement Q Learning - LunarLander-V2
+
+
+Trong project này, mình cài đặt và ứng dụng mô hình Deep Q-Learning học chơi game LunarLander-V2. Kiến trúc mô hình là một mạng CNN đơn giản xây dựng trên module nn (Pytorch). Và sử dụng kỹ thuật Experience Replay cho quá trình chọn mẫu huẩn luyện. 
+
+# Tài liệu mô tả hoạt động
+```
+/document/doc/mota_hoatdong.docx
+/document/doc/presentation.pptx
+```
+# Clip demo trước, giữa và sau khi train
+```
+/demo/clipdemo.zip
+```
+
+# Cài Đặt Môi Trường
+Để cài đặt môi trường gõ lệnh sau
+```
+pip install -r requirements.txt
+```
+
+
+# Tiến hành huấn luyện và xem quá trình học
+Để cài đặt môi trường gõ lệnh sau
+```
+python /source/main.py
+```
+
+# Liên hệ
+Nếu bạn có bất kì vấn đề gì, vui lòng tạo issue hoặc liên hệ mình tại iamhuynguyen1002@gmail.com 
---
+++ source/main.py
@@ -0,0 +1,32 @@
+from lib import *
+from agent import Agent
+
+if __name__ == "__main__":
+    env = gym.make("LunarLander-v2")
+    agent = Agent(gamma=0.99, epsilon=1.0, batch_size=64, n_actions=4, eps_end=0.01, input_dims=[8], lr=0.003)
+    scores, eps_history = [], []
+    n_games = 500
+
+    for i in range(n_games):
+        score = 0
+        done = False
+        observation = env.reset()
+        while not done:
+            env.render()
+            action = agent.choose_action(observation)
+            observation_, reward, done, info = env.step(action)
+            score += reward
+            agent.store_transition(observation, action, reward, observation_, done)
+            agent.learn()
+            observation = observation_
+        scores.append(score)
+        eps_history.append(agent.epsilon)
+        avg_score = np.mean(scores[-100:])
+
+        print('episode ', i, 'score %.2f' % score,
+              'avarage score %.2f' % avg_score,
+              'epsilon %.2f' % agent.epsilon)
+
+
+
+
